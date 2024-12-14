f57211f8e7742a6b037c4bcb524a680674cf7dcf
jeongeui choi <danth@MacBook-Pro.local>
2024-12-14 19:07:06 +0900

    first commit

---
+++ src/main/java/com/stocktide/stocktideserver/StocktideserverApplication.java
@@ -0,0 +1,13 @@
+package com.stocktide.stocktideserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocktideserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocktideserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stocktide/stocktideserver/StocktideserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stocktide.stocktideserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocktideserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
